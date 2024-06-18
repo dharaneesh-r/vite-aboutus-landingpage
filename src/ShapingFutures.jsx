d6555c871be87344d2260940b9abd53fb4ee3215
dharaneesh-r <investoproweal@gmail.com>
2024-06-18 16:32:52 +0530

    changes

--- src/ShapingFutures.jsx
+++ src/ShapingFutures.jsx
@@ -5,22 +5,27 @@ import { useEffect } from "react"
 gsap.registerPlugin(ScrollTrigger)
 
 function ShapingFutures() {
+  let mm = gsap.matchMedia();
+
   useEffect(() => {
-    gsap.fromTo('.shapingfutures-title',{
-      opacity : 0,  
-      x : -40,
-    },{
-      opacity : 1,
-      x : 0,
-      scrollTrigger : {
-        trigger : '.shapingfutures-title',
-        toggleActions : 'restart none none none',
-        start : '10% bottom',
-        duration : 1,
-        scrub : 1,
-        stagger : 1,  
-      }
-    })
+    mm.add("(min-width: 800px)", () => {
+      // desktop setup code here...
+      gsap.fromTo('.shapingfutures-title',{
+        opacity : 0,  
+        x : -40,
+      },{
+        opacity : 1,
+        x : 0,
+        scrollTrigger : {
+          trigger : '.shapingfutures-title',
+          toggleActions : 'restart none none none',
+          start : '10% bottom',
+          duration : 1,
+          scrub : 1,
+          stagger : 1,  
+        }
+      })
+    });
   },[])
   return (
     <div className="shapingfutures-container">
