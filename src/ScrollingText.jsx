690196fbfeeb62b7e8e62cc44934626071a0a583
dharaneesh-r <investoproweal@gmail.com>
2024-06-19 16:35:25 +0530

    changes

--- src/ScrollingText.jsx
+++ src/ScrollingText.jsx
@@ -1,7 +1,7 @@
 function ScrollingText() {
   return (
     <>
-    <div style={{textAlign : 'center',position : 'relative',top : '95px',left : '10px'}}>
+    <div style={{textAlign : 'center',position : 'relative',top : '95px'}}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           focusable="false"
